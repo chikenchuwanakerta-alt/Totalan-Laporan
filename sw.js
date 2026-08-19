b6ec49ba37263402e080b23fc532b2c1f4cdd571
chikenchuwanakerta-alt <chikenchuwanakerta@gmail.com>
2026-08-20 03:46:21 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE="totalan-laporan-v1";
-self.addEventListener("install",e=>self.skipWaiting());
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>{
- if(e.request.method!=="GET") return;
- e.respondWith(fetch(e.request).then(r=>{
-   const c=r.clone(); caches.open(CACHE).then(x=>x.put(e.request,c)); return r;
- }).catch(()=>caches.match(e.request)));
-});
