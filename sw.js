c036a2fc2f40982e737e8fea0ae31e1b070ac8ee
chikenchuwanakerta-alt <chikenchuwanakerta@gmail.com>
2026-08-20 03:47:58 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+/* Totalan Laporan - Service Worker */
+const CACHE_NAME = "totalan-laporan-v1";
+
+const PRECACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE).catch(() => {}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+/* Fetch handler wajib agar Chrome Android menganggap app installable */
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      const network = fetch(req)
+        .then((res) => {
+          if (res && res.ok && req.url.startsWith(self.location.origin)) {
+            const copy = res.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(req, copy)).catch(() => {});
+          }
+          return res;
+        })
+        .catch(() => cached);
+      return cached || network;
+    })
+  );
+});
